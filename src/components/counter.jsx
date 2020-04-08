56cf4d70979ad44b490f99d60f064ad3277f520d
christian baidoo <chris_pens@hotmail.co.uk>
2020-04-08 14:57:00 +0100

    created counters component

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,35 +2,48 @@ import React, { Component } from 'react'
 
 class Counter extends Component {
     state = {
-        count: 0
+        value: this.props.value,
+        tags: ["tag1", "tag2", "tag3", "tag4",]
+    }
+    
+    renderTags = () => {
+
+       return this.state.tags.length === 0 ? <p>There are no tags</p> : <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)} </ul>
+
     }
 
     increment = () => {
         this.setState({
-            count: this.state.count + 1
+            value: this.state.value + 1
         })
     }
 
     decrement = () =>{
         this.setState({
-            count: this.state.count - 1
+            value: this.state.value - 1
         })
     }
 
+  
+
     getbadgeClasses ()  {
         let classes = "badge badge-";
-        classes += this.state.count === 0 ? "warning" : "primary";
+        classes += this.state.value === 0 ? "warning" : "primary";
         return classes;
     }
 
+
+
     render() { 
+        console.log('thjkdfd', this.props)
+      
+
         return (
             <React.Fragment>
-                <h1>Hello Word</h1>
-                <span className={this.getbadgeClasses()}>{this.state.count}</span>
-
+                <span className={this.getbadgeClasses()}>{this.state.value}</span>
                 <button className="btn btn-primary m-2" onClick={this.increment}>Increment</button> 
-                <button className="btn btn-secondary" onClick={this.decrement}>Decrement</button>     
+                {/* <button className="btn btn-secondary" onClick={this.decrement}>Decrement</button>     */}
+                <button onClick={() => this.props.handleDelete(this.props.id)} className="btn btn-danger btn-small m-2">Delete</button>
             </React.Fragment>
         );
 
